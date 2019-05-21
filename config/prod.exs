68f5f38a175e7cc9b6caf99f16c56f3d14e1ee27
Desmond Bowe <desmond@crevalle.io>
2019-05-21 15:06:08 -0700

    Config

--- config/prod.exs
+++ config/prod.exs
@@ -10,9 +10,13 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :soda, SodaWeb.Endpoint,
-  http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [scheme: "https", host: "soda-photos.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  secret_key_base: System.get_env("DATABASE_URL")
+
+config :soda, Soda.Repo,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 10
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -68,4 +72,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which should be versioned
 # separately.
-import_config "prod.secret.exs"
+
